cd2785a15c5454ce447c116f6b04ec06835576b5
nsakib9 <sakibkhan207@gmail.com>
2022-01-24 17:56:28 +0600

    add post update

--- client/src/AddPost.js
+++ client/src/AddPost.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import PostItem from './PostItem';
+import React, { useState } from 'react';
+// import PostItem from './PostItem';
 
 function AddPost(){
+    const [title, settitle] = useState('')
+    const [imageurl, setimageurl] = useState('')
+    const [description, setdescription] = useState('')
+
+    function addpost() {
+        var post = {
+            title: title,
+            imageurl: imageurl,
+            description: description
+        }
+        console.log(post)
+
+    }
+
     return(
         <div>
             <div className="row justify-content-center">
             <div className="col-md-6">
-            <input type="text" placeholder='Title' className='m-2 form-control' />
-            <input type="text" placeholder='Image Url' className='m-2 form-control' />
-            <textarea class="form-control m-2 " cols="30" rows="3"></textarea>
-            <button className="btn btn-success">Add Post</button>
+            <input value={title} onChange={(e) => {settitle(e.target.value)}} type="text" placeholder='Title' className='m-2 form-control' />
+            <input value={imageurl}  onChange={(e) => {setimageurl(e.target.value)}} type="text" placeholder='Image Url' className='m-2 form-control' />
+            <textarea value={description}  onChange={(e) => {setdescription(e.target.value)}} className="form-control m-2 " cols="30" rows="3"></textarea>
+            <button onClick={addpost} className="btn btn-success">Add Post</button>
             </div>
             </div>
         </div>
--- conn.js
+++ conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://ns_user:reactjs20@reactjs20.yonzf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://ns_user:reactjs20@reactjs20.yonzf.mongodb.net/js20blog?retryWrites=true&w=majority');
 
 const dbobject = mongoose.connection
 dbobject.on('connected', () => {console.log("Mongo DB connection success")})
