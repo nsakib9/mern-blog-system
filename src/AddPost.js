93a43d488d1e1df5741eec41860b56d89c2591e3
nsakib9 <sakibkhan207@gmail.com>
2022-01-24 17:18:55 +0600

    Design add post

--- src/AddPost.js
+++ src/AddPost.js
@@ -4,8 +4,14 @@ import PostItem from './PostItem';
 function AddPost(){
     return(
         <div>
-            <h1> This is Add Post</h1>
-            <PostItem />
+            <div className="row justify-content-center">
+            <div className="col-md-6">
+            <input type="text" placeholder='Title' className='m-2 form-control' />
+            <input type="text" placeholder='Image Url' className='m-2 form-control' />
+            <textarea class="form-control m-2 " cols="30" rows="3"></textarea>
+            <button className="btn btn-success">Add Post</button>
+            </div>
+            </div>
         </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -9,7 +9,15 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-     <h2> CRUD Application Using MERN</h2>
+     <h2 style={{backgroundColor: 'red', color: 'white'}}> CRUD Application Using MERN</h2>
+
+<nav class="navbar navbar-light bg-light">
+  <div class="container">
+  <a class="navbar-brand" href="/">Post List</a>
+  <a class="navbar-brand" href="/add">Add Post</a>
+  <a class="navbar-brand" href="/edit">Edit Post</a>
+  </div>
+</nav>
      <BrowserRouter>
      <Routes>
        <Route path="/" element={<PostList/>} />
